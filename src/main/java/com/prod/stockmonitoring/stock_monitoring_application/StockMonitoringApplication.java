77e97f73e1f274f11b7947aa54bfbc3c7b2164b5
shubham <sg5781@srmist.edu.in>
2025-05-26 14:18:04 +0530

    Initial commit for portfolio module

---
+++ src/main/java/com/prod/stockmonitoring/stock_monitoring_application/StockMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.prod.stockmonitoring.stock_monitoring_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prod/stockmonitoring/stock_monitoring_application/StockMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prod.stockmonitoring.stock_monitoring_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
