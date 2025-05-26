905e9c22da3bdb94c299b65a6d95b060b4b55788
animishradke <animishr1975@gmail.com>
2025-05-26 12:16:18 +0530

    msg

---
+++ src/main/java/com/prod/stockmonitor/stock_portfolio_monitor/StockPortfolioMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.prod.stockmonitor.stock_portfolio_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioMonitorApplication.class, args);
+	}
+
+}
