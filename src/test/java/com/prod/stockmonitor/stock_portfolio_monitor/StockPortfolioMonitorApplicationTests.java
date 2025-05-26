af36df5348a2b75ab710b7e8c267cdbf9fd37d06
VACHAN SHETTY <vachan316@gmail.com>
2025-05-26 10:52:50 +0530

    Initial commit

---
+++ src/test/java/com/prod/stockmonitor/stock_portfolio_monitor/StockPortfolioMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prod.stockmonitor.stock_portfolio_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPortfolioMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
